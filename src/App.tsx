5b658682bde98076b51633f75a0218fcddcd2e58
b-rob32 <brendanprobinson@gmail.com>
2025-06-29 06:40:42 -0500

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { initFirebase, authInstance } from './utils/firebase'; // Import firebas
 import LobbyScreen from './components/LobbyScreen';
 import FastestFingerScreen from './components/FastestFingerScreen';
 import GameScreen from './components/GameScreen';
-import SinglePlayerGameScreen from './components/SinglePlayerGameScreen'; // New import
+import SinglePlayerGameScreen from './components/SinglePlayerGameScreen'; // Corrected: Import SinglePlayerGameScreen
 
 export default function App() {
   const [roomId, setRoomId] = useState<string | null>(null);
